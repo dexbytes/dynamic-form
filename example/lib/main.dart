dc320641ec9297b0458cccd60bacc69c733f8dcf
Dinesh pal <dineshpal.dexbytes@gmail.com>
2022-06-28 12:47:29 +0530

    RES-5

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -92,15 +92,13 @@ class _MyFormState extends State<MyForm> {
                     String? jsonString = await ConfigurationSetting.instance.getFormDataLocal();
                     if(jsonString!.isEmpty) {
                       String jsonStringResponse = await httpService.getPosts();
+
                       // String jsonStringResponse = await localJsonRw.localRead();
                       jsonString = await ConfigurationSetting.instance.storeFormDataLocal(jsonStringResponse);
-                      // jsonString = await localJsonRw.localRead();
                     }
-                    //String jsonStringResponse = await httpService.getPosts();
                   setState(() {
                     isLoading = false;
                   });
-
                     String time = timeDuration.endTime(message:"Api calling",reset: true);
                     Navigator.push(
                       context,
